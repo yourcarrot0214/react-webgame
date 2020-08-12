3298836770cd77fe7839f4e4c4700ded814d0112
your Carrot <stylenbs@gmail.com>
2020-08-12 21:47:52 +0900

    Functional Component

--- lecture/WordRelay.jsx
+++ lecture/WordRelay.jsx
@@ -1,52 +1,41 @@
 const React = require("react");
-const { Component } = React;
+const { useState, useRef } = React;
 
-class WordRelay extends Component {
-  state = {
-    word: "자동차",
-    value: "",
-    result: "",
+const WordRelay = () => {
+  const [word, setWord] = useState("자동차");
+  const [value, setValue] = useState("");
+  const [result, setResult] = useState("");
+  const inputRef = useRef(null);
+
+  const onChange = (e) => {
+    setValue(e.target.value);
   };
 
-  onChange = (e) => {
-    this.setState({
-      value: e.target.value,
-    });
-  };
-
-  onSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
 
-    if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
-      this.setState({
-        result: "정답!",
-        word: this.state.value,
-        value: "",
-      });
+    if (word[word.length - 1] === value[0]) {
+      setResult("정답입니다.");
+      setWord(value);
+      setValue("");
+      inputRef.current.focus();
     } else {
-      this.setState({
-        result: "오답!",
-        value: "",
-      });
+      setResult("오답입니다.");
+      setValue("");
+      inputRef.current.focus();
     }
   };
 
-  render() {
-    return (
-      <>
-        <div>{this.state.word}</div>
-        <form onSubmit={this.onSubmit}>
-          <input
-            type="text"
-            value={this.state.value}
-            onChange={this.onChange}
-          />
-          <button type="submit">submit</button>
-        </form>
-        <div>{this.state.result}</div>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <div>{word}</div>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={state.value} onChange={onChange} />
+        <button type="submit">click!</button>
+      </form>
+      <div>{result}</div>
+    </>
+  );
+};
 
 module.exports = WordRelay;
